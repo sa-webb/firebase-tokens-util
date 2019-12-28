8f2f6b88dce5dcd2f31e21da3df33cf25822ade5
sa-webb <sawebb30@gmail.com>
2019-12-28 12:02:29 -0600

    additional reqs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,3 +14,23 @@ exports.get_user = async (req, res) => {
     console.log('Error fetching user data:', error);
   });
 }
+
+exports.grant_admin = async (req, res) => {
+  let uid = req.params.id;
+  admin.auth().setCustomUserClaims(uid, {admin: true}).then(() => {
+    res.status(200).json({ message: 'updated user'})
+  })
+}
+
+exports.check_admin = async (req, res) => {
+  let uid = req.params.id;
+  admin.auth().getUser(uid).then((userRecord) => {
+    if (userRecord.customClaims.admin === true) {
+      res.status(200).json({ message: 'Is admin'})
+      console.log('Is admin');
+    } else {
+      res.status(200).json({ message: 'Is not admin'})
+      console.log('Not admin');
+    }
+  })
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,5 +8,7 @@ const router = express.Router();
 const user_controller = require("../controllers/user.controller");
 
 router.get('/user/:id', user_controller.get_user);
+router.get('/update/:id', user_controller.grant_admin);
+router.get('/isadmin/:id', user_controller.check_admin);
 
 module.exports = router;
